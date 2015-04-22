e68a9c64acbdc1bb4ca8e369d0888ec32d8181ff
TehNut <nick_ignoffo@yahoo.com>
2015-04-22 04:48:27 -0700

    Heh... Did I really remove that?

--- src/main/java/tehnut/launchgui/LaunchGui.java
+++ src/main/java/tehnut/launchgui/LaunchGui.java
@@ -14,6 +14,7 @@ import java.io.File;
 public class LaunchGui {
 
     @Mod.Instance
+    public static LaunchGui instance;
     public static Configuration config;
 
     public static String remoteVersion;
