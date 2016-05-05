268726242621bedc44c07408bc8fd42f2e0c64a2
Nicholas Ignoffo <speedynutty68@gmail.com>
2016-05-04 20:37:26 -0700

    Only load in client environment

--- src/main/java/tehnut/launchgui/LaunchGui.java
+++ src/main/java/tehnut/launchgui/LaunchGui.java
@@ -1,7 +1,6 @@
 package tehnut.launchgui;
 
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import tehnut.launchgui.utils.EventHandler;
@@ -9,24 +8,22 @@ import tehnut.launchgui.utils.Utils;
 
 import java.io.File;
 
-@Mod(modid = LaunchGui.ID, name = LaunchGui.NAME, version = LaunchGui.VERSION)
+@Mod(modid = LaunchGui.ID, name = LaunchGui.NAME, version = LaunchGui.VERSION, clientSideOnly = true)
 public class LaunchGui {
 
     public static final String NAME = "LaunchGui";
     public static final String ID = "launchgui";
-    public static final String CHANNEL = "LaunchGui";
     public static final String VERSION = "@VERSION@";
 
     @Mod.Instance
     public static LaunchGui instance;
-    public static Configuration config;
 
     public static String remoteVersion;
     public static String remoteText;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-        ConfigHandler.init(new File(event.getModConfigurationDirectory() + "/LaunchGui.cfg"));
+        ConfigHandler.init(new File(event.getModConfigurationDirectory(), "LaunchGui.cfg"));
 
         if (ConfigHandler.enableUpdateChecker)
             remoteVersion = Utils.getRemoteVersion();
@@ -34,7 +31,6 @@ public class LaunchGui {
         if (ConfigHandler.enableNoticeGui)
             remoteText = Utils.getRemoteText();
 
-        if (event.getSide().isClient())
-            MinecraftForge.EVENT_BUS.register(new EventHandler());
+        MinecraftForge.EVENT_BUS.register(new EventHandler());
     }
 }
